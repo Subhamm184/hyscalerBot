2629b7fd33004f83f234b13f599d9c984f6b047d
Subhamm184 <157467670+Subhamm184@users.noreply.github.com>
2024-12-13 16:03:57 +0530

    Update chatbot_user.py

--- models/chatbot_user.py
+++ models/chatbot_user.py
@@ -1,23 +1,19 @@
 from odoo import models, fields
 
-class ChatbotUser(models.Model):
-    _name = 'chatbot.user'
-    _description = 'Chatbot User Information'
+class RecruitmentBotUser(models.Model):
+    _name = 'recruitment.bot.user'
+    _description = 'User Data for Recruitment Chatbot'
 
-    name = fields.Char('User Name')
-    company = fields.Many2one('res.company', string='Company')
-    profession = fields.Selection([('trainee', 'Trainee'), ('experienced', 'Experienced')], string='Profession')
-    skills = fields.Char('Skills')
-    quiz_score = fields.Float('Quiz Score')
-    eligible_to_apply = fields.Boolean('Eligible to Apply')
 
-    def calculate_score(self, answers):
-        score = 0
-        for answer in answers:
-            if answer.is_correct:
-                score += 10
-        self.quiz_score = score
-        if score > 70:
-            self.eligible_to_apply = True
-        else:
-            self.eligible_to_apply = False
+    user_name = fields.Char('Name')
+    selected_company = fields.Many2one('res.company', string='Company')
+    role = fields.Selection([('intern', 'Intern'), ('professional', 'Professional')], string='Role')
+    expertise = fields.Char('Key Skills')
+    assessment_score = fields.Float('Assessment Score')
+    is_eligible = fields.Boolean('Eligible for Application', default=False)
+
+
+    def evaluate_eligibility(self, responses):
+        total_score = sum(10 for response in responses if response.is_correct)
+        self.assessment_score = total_score
+        self.is_eligible = total_score >= 70
